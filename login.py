88b6b7826ec66834964df9b715061a18f485ae11
MingSeng-W <2498242826@qq.com>
2016-07-07 11:36:08 +0800

    完成微博登陆,获取cookie

--- login.py
+++ login.py
@@ -3,30 +3,23 @@ import sys
 import re
 import bs4
 import os
-# import MySQLdb
+import mysql.connector
+import mysql
 import urllib
-from urllib2 import HTTPError, URLError
 import urllib2
+from urllib2 import HTTPError, URLError
 import hashlib
 import base64
 import rsa
 import binascii
 import time
-
 import cookielib
-
-
 # 登陆微博
 class weiboLogin:
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}
-    cj = cookielib.LWPCookieJar()
-    cookie_support = urllib2.HTTPCookieProcessor(cj)
-    opener = urllib2.build_opener(cookie_support)
-    urllib2.install_opener(opener)
-
     def get_page(self):
-        url = 'http://login.weibo.cn/login/?ns=1&revalid=2&backURL=http%3A%2F%2Fweibo.cn%2F&backTitle=%CE%A2%B2%A9&vt='
+        url = 'http://login.weibo.cn/login/'
         try:
             req = urllib2.Request(url=url, headers=self.header)
             res = urllib2.urlopen(req)
@@ -36,14 +29,15 @@ class weiboLogin:
             vk = soup.find(attrs={"name": "vk"})['value']
             capId = soup.find(attrs={"name": "capId"})['value']
             imglink = soup.find('img')['src']
+            tempurl=soup.find('form')['action']
+            logurl='http://login.weibo.cn/login/'+tempurl
             urllib.urlretrieve(imglink, '验证码.jpg')
-            return password, vk, capId
+            return password, vk, capId,logurl
         except URLError, e:
             if hasattr(e, 'reason'):
                 print  e.reason
             elif hasattr(e, 'code'):
                 print e.code
-
     def get_account(self):
         username = ""
         password = ""
@@ -56,25 +50,57 @@ class weiboLogin:
                 else:
                     password = line.strip()
         return username, password
+    def get_login(self):
+        passowrd_temp,vk,capId,logurl=self.get_page()
+        code = raw_input("please input the code:")
+        mobile,password=self.get_account()
+        data={
+            "mobile":mobile,
+            "tryCount":"",
+            "vk":vk,
+            "capId":capId,
+            "code":code,
+            "backTtile":"http%3A%2F%2Fweibo.cn%2F",
+            "backURL":"微博",
+            "remember":"on",
+            "submit":"登录"
+        }
+        data[passowrd_temp]=password
+        data2=urllib.urlencode(data)
+        try:
+            filename='cookie.txt'
+            req=urllib2.Request(url=logurl,headers=self.header)
+            cookie= cookielib.MozillaCookieJar(filename)
+            cookie_support = urllib2.HTTPCookieProcessor(cookie)
+            opener = urllib2.build_opener(cookie_support)
+            res=opener.open(req,data2)
+            cookie.save(filename,ignore_discard=True,ignore_expires=True)
+            # cj2=cookielib.MozillaCookieJar()
+            # cj2.load('cookie.txt',ignore_discard=True,ignore_expires=True)
+            # opener2=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj2))
+            # req2=urllib2.Request('http://weibo.cn/?since_id=0&max_id=DDyTmzAV2&prev_page=1&page=2&vt=4',headers=self.header)
+            # res2=opener2.open(req2)
+            # html=res2.read()
+            # print html
+        except URLError,e:
+            if hasattr(e,'reason'):
+                print e.reason
+            if hasattr(e,'code'):
+                print e.code
+    def get_user(self):
 
-        # def start_login(self):
-        # username,passowrd=self.get_account()
-        # prepassowrd,vk,capId=self.get_page()
-        # code=raw_input('请输入验证码:')
-        # print code
 
-        # data={
-        #     'mobile':username,
-        #     'code':code,
-        #     'vk':vk,
-        #     'capId':capId,
-        #     'submit':'登录',
-        #     'tryCount':'',
-        #     'remember':'on',
-        #     'backURL':'http%3A%2F%2Fweibo.cn%2F',
-        #     'backTitle':'微博'
-        # }
-        # data[prepassowrd]=passowrd
+wb=weiboLogin()
+wb.get_login()
+
+
+
+
+
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import re
 import bs4
 import os
+import mysql.connector
 import mysql
 import urllib
 import urllib2
@@ -9,14 +10,39 @@ import time
 
 import cookielib
 class weiboCrawlser:
+
     cj = cookielib.LWPCookieJar()
     cookie_support = urllib2.HTTPCookieProcessor(cj)
     opener = urllib2.build_opener(cookie_support)
     urllib2.install_opener(opener)
+    def connectMysql(self,username,userid,weibotext,weiboid):
+        config = {
+            'user': 'root',
+            'password': '',
+            'host': 'localhost',
+            'port': '3306',
+            'database': 'weibostore'
+        }
+        conn = mysql.connector.connect(**config)
+        cursor = conn.cursor()
+        add_user = (
+        "INSERT INTO user(userid,username,weiboid,weibocontent) VALUES(%(userid)s,%(username)s,%(weiboid)s,%(weibocontent)s)")
+        data_user = {
+            'userid': userid,
+            'username': username,
+            'weiboid': weiboid,
+            'weibocontent': weibotext
+        }
 
-    def test(self, pagenum):
+        cursor.execute(add_user, data_user)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+    def test(self, pagenum,keyword):
         print 'downloading page' + str(pagenum)
-        url = 'http://s.weibo.com/weibo/sss&page=' + str(pagenum)
+        url = 'http://s.weibo.com/weibo/'+keyword+'&page=' + str(pagenum)
         header = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
             'Cookie': 'SINAGLOBAL=7794664723802.307.1464800947011; _s_tentry=-; Apache=8258607474181.003.1467273666438; ULV=1467273666448:5:5:2:8258607474181.003.1467273666438:1467262664430; SWB=usrmdinst_14; NSC_wjq_txfjcp_mjotij=ffffffff094113d645525d5f4f58455e445a4a423660; UOR=www.liaoxuefeng.com,widget.weibo.com,www.baidu.com; appkey=; WBtopGlobal_register_version=c5a1a241471e96ea; SUB=_2A256cgCoDeTxGeNL7FUZ-CfJzT2IHXVZBnVgrDV8PUJbmtBeLWLEkW8g9LCVu3mmtEhqMNpCWlKRFm8QSA..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5MINdBKrimGBVg4AxBhZ3J5JpX5K2hUgL.Fo-fS0MR1h.fSo22dJLoI02LxK-LBKzLBKnLxK.L1K-L1-eLxK-L1K-LBKqLxK-L1K-LBKqLxK-L1K-LBKq_eh50; SUHB=0OMUZMcoeOe70z; SSOLoginState=1467379960; un=15520441791; WBStore=8ca40a3ef06ad7b2|undefined'
@@ -35,6 +61,7 @@ class weiboCrawlser:
             newthml = f2.read()
             newthmls = bs4.BeautifulSoup(newthml, 'lxml')
             imgAry = newthmls.find_all('img', attrs={'action-type': 'fl_pics'})
+            print imgAry
             # 获取微博的用户名,微博id,用户名id,微博内容
             blogs = newthmls.find_all(attrs={'action-type': 'feed_list_item'})
             for i in range(0, len(blogs)):
@@ -45,16 +72,16 @@ class weiboCrawlser:
                 weiboId = re.findall('refer_flag=(.*)', useridLink)[0]
                 try:
                     userid = re.findall('http:\/\/weibo.com\/u\/(.*?)\?', useridLink)[0]
-                    print userid, username, weiboId, weiboText
+                    self.connectMysql(username,userid,weiboText,weiboId)
+                    # print userid, username, weiboId, weiboText
                 except:
                     useridtemp = re.sub(r'http://weibo.com/(.*?)', '', useridLink)
                     useridtemp2 = re.sub('/?refer_flag', '', useridtemp)
                     userid = re.findall(r'(^\w*)', useridtemp2)[0]
-                    print userid, username, weiboId, weiboText
+                    # print userid, username, weiboId, weiboText
             path = os.path.abspath('.')
             imgpath = path + '/images/'
             totalImageNum = int([len(files) for root, dirs, files in os.walk(imgpath)][0])
-
             for index in range(0, len(imgAry)):
                 imglink = imgAry[index]['src']
                 imgname = './images/' + str(index + totalImageNum) + '.jpg'
@@ -62,9 +89,41 @@ class weiboCrawlser:
             print 'finsh download page' + str(pagenum)
 
 
+
+def connectMysql(username,userid,weibotext,weiboid):
+    config={
+        'user':'root',
+        'password':'',
+        'host':'localhost',
+        'port':'3306',
+        'database':'weibostore'
+    }
+    conn=mysql.connector.connect(**config)
+    cursor=conn.cursor()
+    add_user=("INSERT INTO user(userid,username,weiboid,weibocontent) VALUES(%(userid)s,%(username)s,%(weiboid)s,%(weibocontent)s)")
+    data_user={
+        'userid':userid,
+        'username':username,
+        'weiboid':weiboid,
+        'weibocontent':weibotext
+    }
+
+    cursor.execute(add_user,data_user)
+    conn.commit()
+    result_set=cursor.fetchone()
+    print result_set
+    cursor.close()
+    conn.close()
+
+
+
+
+
+
+keyword=raw_input("please in put the keyword:")
 wb = weiboCrawlser()
-for i in range(1, 3):
-    wb.test(i)
+for i in range(1, 7):
+    wb.test(i,keyword)
     time.sleep(10)
 print 'finish all'
 
