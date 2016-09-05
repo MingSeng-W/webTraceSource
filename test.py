205d3de318348b578ef00f62f53f942e86662439
MingSeng-W <2498242826@qq.com>
2016-09-05 09:28:43 +0800

    获得第一个时间的版本做测试

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ class weiboCrawlser:
     cookie_support = urllib2.HTTPCookieProcessor(cj)
     opener = urllib2.build_opener(cookie_support)
     urllib2.install_opener(opener)
+    index=1
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
         'Cookie':'SINAGLOBAL=2580452964109.3633.1468546273913; _s_tentry=www.doc88.com; TC-Page-G0=9183dd4bc08eff0c7e422b0d2f4eeaec; Apache=5174620901646.758.1472833693191; ULV=1472833693197:5:1:1:5174620901646.758.1472833693191:1471356725919; TC-Ugrow-G0=e66b2e50a7e7f417f6cc12eec600f517; TC-V5-G0=1e4d14527a0d458a29b1435fb7d41cc3; login_sid_t=33ce6566732e1ba914cfaa364629f553; YF-Ugrow-G0=5b31332af1361e117ff29bb32e4d8439; YF-V5-G0=a5a264208a5b5a42590274f52e6c7304; YF-Page-G0=416186e6974c7d5349e42861f3303251; un=15025019730; UOR=www.micmiu.com,widget.weibo.com,login.sina.com.cn; SCF=AvT6HCtAMdOj-c5qkVuiYsW5iilNltQh3db0hjO7i625vWzLW1imdDWEXEN7EseMy549ayrBfg5NnhACn2gwT3E.; SUB=_2A256z4lGDeTxGeNL7FUZ-CfJzT2IHXVZvP2OrDV8PUNbmtBeLXWikW9Y-RlzQQMLo1M4csiverelxsWFsA..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5MINdBKrimGBVg4AxBhZ3J5JpX5KzhUgL.Fo-fS0MR1h.fSo22dJLoI02LxK-LBKzLBKnLxK.L1K-L1-eLxK-L1K-LBKqLxK-L1K-LBKqLxK-L1K-LBKq_eh50; SUHB=0ftZjw-62RxhjA; ALF=1504521366; SSOLoginState=1472985366; wvr=6'
@@ -34,15 +35,16 @@ class weiboCrawlser:
         conn = mysql.connector.connect(**config)
         cursor = conn.cursor()
         add_user = (
-        "INSERT INTO wieboid(username,link,weibotext,time,date) VALUES(%(username)s,%(link)s,%(weibotext)s,%(time)s,%(date)s)")
+        "INSERT INTO user(username,link,weibotext,time,date,myindex) VALUES(%(username)s,%(link)s,%(weibotext)s,%(time)s,%(date)s,%(myindex)s)")
         data_user = {
             'link': link,
             'username': username,
             'weibotext': weibotext,
             'time': time,
-            'date':date
+            'date':date,
+            'myindex':str(self.index)
         }
-
+        self.index=self.index+1
         cursor.execute(add_user, data_user)
         conn.commit()
         cursor.close()
@@ -68,36 +70,7 @@ class weiboCrawlser:
                 r = i.encode('utf-8').decode('unicode_escape').encode('utf-8')
                 s = r.replace("\/", "/")
                 f.write(s)
-        # with open('test.html', 'r') as f2:
-        #     newthml = f2.read()
-        #     newthmls = bs4.BeautifulSoup(newthml, 'lxml')
-        #     imgAry = newthmls.find_all('img', attrs={'action-type': 'fl_pics'})
-        #     print imgAry
-        #     # 获取微博的用户名,微博id,用户名id,微博内容
-        #     blogs = newthmls.find_all(attrs={'action-type': 'feed_list_item'})
-        #     for i in range(0, len(blogs)):
-        #         temp = blogs[i].find(attrs={'class': 'W_texta W_fb'})
-        #         username = temp['nick-name']
-        #         useridLink = temp['href']
-        #         weiboText = blogs[i].find(attrs={'class': 'comment_txt'}).get_text().strip()
-        #         weiboId = re.findall('refer_flag=(.*)', useridLink)[0]
-        #         try:
-        #             userid = re.findall('http:\/\/weibo.com\/u\/(.*?)\?', useridLink)[0]
-        #             self.connectMysql(username,userid,weiboText,weiboId)
-        #             # print userid, username, weiboId, weiboText
-        #         except:
-        #             useridtemp = re.sub(r'http://weibo.com/(.*?)', '', useridLink)
-        #             useridtemp2 = re.sub('/?refer_flag', '', useridtemp)
-        #             userid = re.findall(r'(^\w*)', useridtemp2)[0]
-        #             # print userid, username, weiboId, weiboText
-        #     path = os.path.abspath('.')
-        #     imgpath = path + '/images/'
-        #     totalImageNum = int([len(files) for root, dirs, files in os.walk(imgpath)][0])
-        #     for index in range(0, len(imgAry)):
-        #         imglink = imgAry[index]['src']
-        #         imgname = './images/' + str(index + totalImageNum) + '.jpg'
-        #         urllib.urlretrieve(imglink, imgname)
-        #     print 'finsh download page' + str(pagenum)
+
     def getfirstpage(self):
         self.getpage(0,0,'fisrtpage.html')
 
@@ -113,21 +86,52 @@ class weiboCrawlser:
                     pass
             return  totalpage
 
-    def getFirstPerson(self):
+
+
+    # 获得微博页面的数组函数
+    def getweiboPageAry(self,page,filename):
+        self.getpage(1,page,filename)
+        html=self.getBS4Obj(filename=filename)
+        weiboAry=html.find_all(attrs={'class':'WB_cardwrap S_bg2 clearfix'})
+        return weiboAry
+
+    def getWeiboInfo(self,weiboCon):
+        text = weiboCon.find('p', attrs={'class': 'comment_txt'}).get_text()
+        temp = weiboCon.find(attrs={'class': 'face'}).find('a')
+        name = temp['title']
+        link = temp['href']
+        timeTemp = weiboCon.find(attrs={'class': 'feed_from W_textb'}).find('a')
+        mytime = timeTemp['title']
+        date = timeTemp['date']
+        self.connectMysql(username=name, link=link, time=mytime, date=date, weibotext=text)
+
+    def getFirstTime(self, totalpage,filename):
+        temptime=''
+        tmepUnix=''
+        weiboAry=self.getweiboPageAry(totalpage,filename)
+        length=len(weiboAry)-1
+        weibocon=weiboAry[length]
+        temptime=weibocon['title']
+        tmepUnix=weibocon['date']
+        return temptime,tmepUnix
+    def judgePage(self,totalpage):
+
+
+    def getPerson(self):
             totalpage=str(self.getTotalPageNum())
-            self.getpage(1,totalpage,'originalpage.html')
-            html=self.getBS4Obj('originalpage.html')
-            weiboconAry=html.find_all(attrs={'class':'WB_cardwrap S_bg2 clearfix'})
-            last=len(weiboconAry)-1
-            weiboCon=weiboconAry[last]
-            text = weiboCon.find('p', attrs={'class':'comment_txt'}).text
-            temp=weiboCon.find(attrs={'class':'face'}).find('a')
-            name=temp['title']
-            link=temp['href']
-            timeTemp=weiboCon.find(attrs={'class':'feed_from W_textb'}).find('a')
-            time=timeTemp['title']
-            date=timeTemp['date']
-            self.connectMysql(username=name,link=link,time=time,date=date,weibotext=text)
+            totalpagetemp=int(totalpage)
+            print "totalpage",totalpage
+            if totalpage
+            for i in range(1,totalpagetemp+1):
+                self.getpage(1,str(i),'originalpage.html')
+                html=self.getBS4Obj('originalpage.html')
+                weiboconAry=html.find_all(attrs={'class':'WB_cardwrap S_bg2 clearfix'})
+                length=len(weiboconAry)
+                for k in range(0,length):
+                        weiboCon=weiboconAry[k]
+                        self.getWeiboInfo(weiboCon)
+                print "finish page",i
+                time.sleep(10)
 
 
 
@@ -135,35 +139,36 @@ class weiboCrawlser:
 
 
 
-def connectMysql(username,userid,weibotext,weiboid):
-    config={
-        'user':'root',
-        'password':'',
-        'host':'localhost',
-        'port':'3306',
-        'database':'weibostore'
-    }
-    conn=mysql.connector.connect(**config)
-    cursor=conn.cursor()
-    add_user=("INSERT INTO user(userid,username,weiboid,weibocontent) VALUES(%(userid)s,%(username)s,%(weiboid)s,%(weibocontent)s)")
-    data_user={
-        'userid':userid,
-        'username':username,
-        'weiboid':weiboid,
-        'weibocontent':weibotext
-    }
-
-    cursor.execute(add_user,data_user)
-    conn.commit()
-    result_set=cursor.fetchone()
-    print result_set
-    cursor.close()
-    conn.close()
+# def connectMysql(username,userid,weibotext,weiboid):
+#     config={
+#         'user':'root',
+#         'password':'',
+#         'host':'localhost',
+#         'port':'3306',
+#         'database':'weibostore'
+#     }
+#     conn=mysql.connector.connect(**config)
+#     cursor=conn.cursor()
+#     add_user=("INSERT INTO user(username,link,weibocontent) VALUES(%(userid)s,%(username)s,%(weiboid)s,%(weibocontent)s)")
+#     data_user={
+#         'userid':userid,
+#         'username':username,
+#         'weiboid':weiboid,
+#         'weibocontent':weibotext
+#     }
+#
+#     cursor.execute(add_user,data_user)
+#
+#     conn.commit()
+#     result_set=cursor.fetchone()
+#     print result_set
+#     cursor.close()
+#     conn.close()
 
 
 keyword=raw_input("please in put the keyword:")
 wb = weiboCrawlser(keyword=keyword)
-wb.getFirstPerson()
+wb.getPerson()
 # for i in range(1, 7):
 #     wb.test(i,keyword)
 #     time.sleep(20)
--- test2.py
+++ test2.py
@@ -1,7 +1,3 @@
-import time
-import re
-timestr=str(time.time())
-newtime=timestr.replace(".","0")
-print newtime
-
+for i in range(1,10):
+    print i
 
